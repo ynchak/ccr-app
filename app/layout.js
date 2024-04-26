6168b609e58bc8219a228a94e49be66dd418051e
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2024-04-26 13:36:08 +0300

    fix font

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="uk" suppressHydrationWarning>
-      <body className={`${GeistSans.className} text-base`}>
+      <body className={`${GeistSans.className}`}>
         <Providers>
           <MainNav />
           {children}
