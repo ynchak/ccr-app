74ef21832ddb09e090163d93e866762517b1f328
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2024-04-30 10:51:22 +0300

    max line infinity

--- configs/app-config.js
+++ configs/app-config.js
@@ -1,4 +1,4 @@
 export const APP_KEY = 'ccr-app';
-export const MAX_LINES = 40;
+export const MAX_LINES = Infinity;
 export const MAX_GOODS_IN_SOURCE = 50;
 export const MAX_GOODS_IN_CONSTRUCT = 500;
