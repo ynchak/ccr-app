0b0ef629715ca9507d3b77ee4f7f8832dc038af6
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2024-10-22 11:02:12 +0300

    Update openInCunstructConfig.js

--- form-data/form-configs/openInCunstructConfig.js
+++ form-data/form-configs/openInCunstructConfig.js
@@ -5,8 +5,8 @@ import { openInNewTab } from '@/utils';
 const addres = `https://gomer.rozetka.company/gomer/smart-folders/next-page?searchParams[0][field]=goods_id&searchParams[0][condition]=%3D&searchParams[0][value]=`;
 
 const params = {
-  all: '&pageCondition=all&page=0&pageSize=50&size=500',
-  active: `&searchParams[1][field]=upload_status&searchParams[1][condition]=equal-value&searchParams[1][value]=2&pageCondition=all&page=0&pageSize=50&size=500`,
+  all: `&pageCondition=all&page=0&pageSize=50&size=${MAX_GOODS_IN_CONSTRUCT}`,
+  active: `&searchParams[1][field]=upload_status&searchParams[1][condition]=equal-value&searchParams[1][value]=2&pageCondition=all&page=0&pageSize=50&size=${MAX_GOODS_IN_CONSTRUCT}`,
 };
 
 const action = (column, type) => {
