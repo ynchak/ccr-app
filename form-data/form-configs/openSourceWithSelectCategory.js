137753d5a5e3cb05c7663f2ad8402e1802032ce2
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2026-01-06 18:15:22 +0200

    Update openSourceWithSelectCategory.js

--- form-data/form-configs/openSourceWithSelectCategory.js
+++ form-data/form-configs/openSourceWithSelectCategory.js
@@ -4,11 +4,11 @@ import { openInNewTab } from '@/utils';
 import { MAX_GOODS_IN_SOURCE, MAX_LINES } from '@/configs/app-config';
 
 const url = 'https://gomer.rozetka.company/gomer/items/source/';
-// const attr = '?rz_category_id=';
-const attr = '';
+const attr = '?rz_category_id=';
 
 const createLink = (source, idCategory) =>
-  `${url}${source}${attr}${idCategory}`;
+  `${url}${source}`;
+  // `${url}${source}${attr}${idCategory}`;
 
 const action = (data) => {
   const { settingsBy, value } = useSettingsStore.getState();
