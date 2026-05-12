5233de4f707e8adbc3bb98635faadb42e0b4ddc5
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2026-05-12 11:31:45 +0300

    Create openInFlanders.js
    open in Flanders

---
+++ form-data/form-configs/openInFlanders.js
@@ -0,0 +1,18 @@
+import { openInNewTab } from '@/utils';
+
+const action = (column, _, setCountLines) => {
+  const links = column
+    .split('\n')
+    .filter((line) => line !== '')
+    .map((id) => `https://gomer.rozetka.company/goods/item/update?id={id}/`);
+  setCountLines(links.length);
+  openInNewTab(links.join('\n'));
+};
+export const openInFlanders = {
+  buttonColor: 'secondary',
+  buttonTitle: 'Відкрити',
+  buttonIcon: 'open',
+  placeholder: 'Сюди треба ввести id товарів',
+  radioGroup: null,
+  action,
+};
