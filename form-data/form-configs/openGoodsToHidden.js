7ea8c4489b06b505299bc49da3893c5c46e49db8
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2026-06-17 11:43:58 +0000

    update

--- form-data/form-configs/openGoodsToHidden.js
+++ form-data/form-configs/openGoodsToHidden.js
@@ -10,15 +10,14 @@ const params = {
   active: `&searchParams[1][field]=upload_status&searchParams[1][condition]=equal-value&searchParams[1][value]=2&pageCondition=all&page=0&pageSize=50&size=${MAX_GOODS_IN_CONSTRUCT}`,
 };
 
-const action = (column, type) => {
-  console.log(column, '\t - ', type);
+const action = (column) => {
 
   const lines = column.split('\n').filter((line) => line !== '');
   const links = chunk(lines, MAX_GOODS_IN_CONSTRUCT).map(
-    (part) => `${addres}${part.join('+')}${params[type]}`
+    (part) => `${addres}${part.join('+')}${params['all']}`
   );
-
-  openInNewTab(links);
+  console.log(links);
+//   openInNewTab(links);
 };
 
 export const openGoodsToHidden = {
